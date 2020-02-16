d513c4e803a651bb3cc569afc6368d29ff2aff90
tomfangcom <tomfang@126.com>
2020-02-16 11:10:17 +0800

    add search api

--- smart-map-web/src/main/resources/db/schema.sql
+++ smart-map-web/src/main/resources/db/schema.sql
@@ -1,11 +1,32 @@
-create table if not exists USER (
-USE_ID int not null primary key auto_increment,
-USE_NAME varchar(100),
-USE_SEX varchar(1),
-USE_AGE NUMBER(3),
-USE_ID_NO VARCHAR(18),
-USE_PHONE_NUM VARCHAR(11),
-USE_EMAIL VARCHAR(100),
-CREATE_TIME DATE,
-MODIFY_TIME DATE,
-USE_STATE VARCHAR(1));
+create table if not exists map_history (
+ID int not null primary key auto_increment,
+USE_ID varchar(100),
+title
+point
+url
+detailUrl
+address
+city
+province
+phoneNumber
+postcode
+distance
+tag
+create_time DATE,
+
+
+
+  title: results[x].Br[i].title,
+                                point: results[x].Br[i].point.lng + "，" + results[x].Br[i].point.lat,
+                                url: results[x].Br[i].url,
+                                detailUrl: results[x].Br[i].detailUrl,
+                                address: results[x].Br[i].address,
+                                city: results[x].Br[i].city,
+                                province: results[x].Br[i].province,
+                                phoneNumber: results[x].Br[i].phoneNumber,
+                                postcode: results[x].Br[i].postcode,
+                                type: results[x].Br[i].type,
+                                isAccurate: results[x].Br[i].isAccurate,
+                                distance: distance,
+                                keyword: results[x].keyword,
+                                tag: 0
